cc9d2ad6e7759cf717e1e33e27b3328286049630
nemza4 <nemavholags@gmail.com>
2018-08-07 13:27:08 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+const firebaseConfig = {
+
+  apiKey: "AIzaSyD2_WFneyd554fmghyiABxcANm3npw_Ql0",
+    authDomain: "firelist-ca17e.firebaseapp.com",
+    databaseURL: "https://firelist-ca17e.firebaseio.com",
+    projectId: "firelist-ca17e",
+    storageBucket: "firelist-ca17e.appspot.com",
+    messagingSenderId: "608896352875"
+  };
 
 
 @NgModule({
